c4045b16de4fa6e8f3d4666029501f639ae7519b
desconocidoxxx <144498068+Anthonyxd22@users.noreply.github.com>
2024-07-24 20:06:33 -0400

    Update run-install.sh

--- run-install.sh
+++ run-install.sh
@@ -2,36 +2,35 @@
 clear
 rm *.bat
 
-prepare_install() {
-    if [ -d ".venv" ]; then
-        echo "Venv found. This implies Applio has been already installed or this is a broken install."
-        echo "Removing existing virtual environment..."
-        rm -rf .venv
-    fi
-    
-    echo "Creating new venv..."
-    
-    requirements_file="requirements.txt"
-    echo "Checking if python exists"
-    
-    if command -v python3.10 > /dev/null 2>&1; then
-        py=$(which python3.10)
-        echo "Using python3.10"
+if [ -d ".venv" ]; then
+    echo "Venv found. This implies Applio has been already installed or this is a broken install."
+    echo "Removing existing virtual environment..."
+    rm -rf .venv
+fi
+
+echo "Creating new venv..."
+
+requirements_file="requirements.txt"
+echo "Checking if python exists"
+
+if command -v python3.10 > /dev/null 2>&1; then
+    py=$(which python3.10)
+    echo "Using python3.10"
+else
+    if command -v python3 > /dev/null 2>&1; then
+        py=$(which python3)
+        echo "Using python3"
     else
-        if command -v python3 > /dev/null 2>&1; then
-            py=$(which python3)
-            echo "Using python3"
-        else
-            echo "Please install Python 3 or 3.10 manually."
-            exit 1
-        fi
+        echo "Please install Python 3 or 3.10 manually."
+        exit 1
     fi
+fi
 
-    $py -m venv .venv
-    . .venv/bin/activate
-    
-    echo "Installing requirements..."
-    pip install -r $requirements_file
-}
+$py -m venv .venv
+. .venv/bin/activate
 
-prepare_install
+echo "Installing requirements..."
+pip install -r $requirements_file
+
+chmod +x run-applio.sh
+./run-applio.sh
