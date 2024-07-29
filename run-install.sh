5a07e5c17299d54f050ed73b1c1a29f3e44351ae
desconocidoxxx <144498068+Anthonyxd22@users.noreply.github.com>
2024-07-28 23:33:45 -0400

    Update run-install.sh test

--- run-install.sh
+++ run-install.sh
@@ -4,12 +4,13 @@ export TERM=xterm
 
 printf "\033]0;Installer\007"
 clear
-rm *.bat
+rm -f *.bat
 
 prepare_install() {
     echo "Creating venv..."
     requirements_file="requirements.txt"
     echo "Checking if Python exists"
+    
     if command -v python3.10 > /dev/null 2>&1; then
         py=$(which python3.10)
         echo "Using Python 3.10"
@@ -35,8 +36,11 @@ prepare_install() {
     echo
     echo "Installing Applio dependencies..."
     python -m pip install -r requirements.txt
+
+    
     python -m pip uninstall torch torchvision torchaudio -y
     python -m pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https://download.pytorch.org/whl/cu121
+    
     pip uninstall urllib3 requests -y
     pip install urllib3 requests
 
@@ -47,11 +51,14 @@ finish() {
     if [ -f "${requirements_file}" ]; then
         installed_packages=$(python -m pip freeze)
         while IFS= read -r package; do
+           
             expr "${package}" : "^#.*" > /dev/null && continue
-            package_name=$(echo "${package}" | sed 's/[<>=!].*//')
-            if ! echo "${installed_packages}" | grep -q "${package_name}"; then
-                echo "${package_name} not found. Attempting to install..."
-                python -m pip install --upgrade "${package}"
+            if [ -n "$package" ]; then
+                package_name=$(echo "${package}" | sed 's/[<>=!].*//')
+                if ! echo "${installed_packages}" | grep -q "${package_name}"; then
+                    echo "${package_name} not found. Attempting to install..."
+                    python -m pip install --upgrade "${package}"
+                fi
             fi
         done < "${requirements_file}"
     else
@@ -69,10 +76,12 @@ finish() {
 if [ "$(uname)" = "Darwin" ]; then
     if ! command -v brew >/dev/null 2>&1; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    else
-        brew install python@3.10
     fi
-elif [ "$(uname)" != "Linux" ]; then
+    brew install python@3.10
+elif [ "$(uname)" = "Linux" ]; then
+    
+    echo "Linux detected. Proceeding with Linux-specific instructions."
+else
     echo "Unsupported operating system. Are you using Windows...?"
     echo "If yes, use the batch (.bat) file instead of this one!"
     exit 1
