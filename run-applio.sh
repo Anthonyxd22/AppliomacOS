6ce4f5f49e8392362a1f8f3311416d9d6d6b8308
desconocidoxxx <144498068+Anthonyxd22@users.noreply.github.com>
2024-10-09 09:52:23 -0400

    Update run-applio.sh

--- run-applio.sh
+++ run-applio.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-
+# Set terminal type
 export TERM=xterm-256color
+
 
 printf "\033]0;Applio\007"
 
-
+# Activate the virtual environment
 . .venv/bin/activate
 
 
 export PYTORCH_ENABLE_MPS_FALLBACK=1
 export PYTORCH_MPS_HIGH_WATERMARK_RATIO=0.0
 
-
+# Clear the terminal
 clear
 
 # Run the application
